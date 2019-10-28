861a7cb0ba0294bdc52d976fabce920eb311dfb6
Matt Benjamin <mbenjamin@redhat.com>
2019-10-28 10:26:50 -0400

    incr
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- tbplus.cxx
+++ tbplus.cxx
@@ -18,53 +18,21 @@
 #include <string>
 #include <vector>
 
-#include "ceph_types.h"
-#include "rgw_cksum.h"
+#include "bplus_leaf.h"
 
 #define dout_subsys ceph_subsys_rgw
 
 namespace {
 
-  using namespace rgw::cksum;
+  using namespace rgw::bplus;
   using std::get;
   using std::string;
 
-  CksumType t1 = CksumType::blake2bp;
-  CksumType t2 = CksumType::sha256;
-
-  std::string dolor =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod "
-    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim "
-    "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea "
-    "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate "
-    "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint "
-    "occaecat cupidatat non proident, sunt in culpa qui officia deserunt "
-    "mollit anim id est laborum.";
   
 }
 
 int main(int argc, char **argv)
 {
-  ceph::crypto::SHA512 hasher;
-  sha512_digest_t hash;
-  hasher.Update((const unsigned char *)dolor.c_str(), dolor.length());
-  hasher.Final(hash.v);
-  
-  for (auto t : {t1, t2}) {
-    DigestVariant dv = rgw::cksum::digest_factory(t);
-    Digest* digest = get_digest(dv);
-
-    if (digest) {
-      digest->Update((const unsigned char *)dolor.c_str(), dolor.length());
-    }
-
-    if (digest) {
-      auto cksum = rgw::cksum::finalize_digest(digest, t);
-      std::cout << "type: " << to_string(t)
-		<< "digest: " << cksum.to_string()
-		<< std::endl;
-    }
-  }
 
   return 0;
 }

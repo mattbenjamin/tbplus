c0ffd7d894dfcc79d33f318d42dd5742ecef3029
Matt Benjamin <mbenjamin@redhat.com>
2019-11-07 17:39:15 -0500

    bplus:  simplify out all pointer indirection (dead end)
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- bplus_leaf.h
+++ bplus_leaf.h
@@ -70,78 +70,52 @@ namespace rgw { namespace bplus {
       NodeType type;
 
       // XXX: likely to change with key prefixing
-      class KeyValueEntry
+      class KVEntry
       {
       public:
 	std::string key;
 	std::string val;
-	KeyValueEntry(const std::string& k, const std::string& v)
+	KVEntry(const std::string& k, const std::string& v)
 	  : key(k), val(v) {}
-	KeyValueEntry(KeyValueEntry&& rhs)
+	KVEntry(KVEntry&& rhs)
 	  : key(rhs.key), val(rhs.val) {}
-	KeyValueEntry& operator=(KeyValueEntry&& rhs) {
+	KVEntry& operator=(KVEntry&& rhs) {
 	  key = rhs.key;
 	  val = rhs.val;
 	  return *this;
 	}
-      }; /* KeyValueEntry */
+      }; /* KVEntry */
 
-      // XXX: likely to change for indirection into local or flatbuffer rep
-      class KeyEntry
-      {
-	friend class Node;
-	size_t rep_off;
-      public:
-	KeyEntry(size_t _rep_off)
-	  : rep_off(_rep_off) {}
-	inline KeyValueEntry& elt(const Node* node) const {
-	  return const_cast<KeyValueEntry&>(node->local_rep[rep_off]);
-	}
-	inline const std::string& key(const Node* node) const {
-	  return elt(node).key;
-	}
-	inline const std::string& val(const Node* node) const {
-	  return elt(node).val;
-	}
-      }; /* KeysViewEntry */
+      vector<KVEntry> data; // unsorted vector of {k,v}
 
-      vector<KeyValueEntry> local_rep; // unsorted vector of {k,v}
-      vector<KeyEntry> keys_view; // sorted indirection vector
-
-      using rep_iterator = decltype(local_rep)::iterator;
-      using keysview_iterator = decltype(keys_view)::iterator;
+      using data_iterator = decltype(data)::iterator;
 
       // indirect compf
       struct KeysViewLT
       {
-      private:
-	const Node* node;
       public:
-	bool operator()(const KeyEntry& lhs, const KeyEntry& rhs) const
-	  { return (lhs.key(node) < rhs.key(node)); }
-	bool operator()(const std::string& k, const KeyEntry& rhs) const
-	  { return (k < rhs.key(node)); }
-	bool operator()(const KeyEntry& lhs, const std::string& k) const
-	  { return (lhs.key(node) < k); }
-	KeysViewLT(const Node* _node)
-	  : node(_node) {}
+	bool operator()(const KVEntry& lhs, const KVEntry& rhs) const
+	  { return (lhs.key < rhs.key); }
+	bool operator()(const std::string& k, const KVEntry& rhs) const
+	  { return (k < rhs.key); }
+	bool operator()(const KVEntry& lhs, const std::string& k) const
+	  { return (lhs.key < k); }
       }; /* KeysViewLT */
 
       KeysViewLT keysviewLT;
 
     public:
       Node()
-	: keysviewLT(this)
 	{}
 
       Node(std::vector<uint8_t> flatv)
-       : keysviewLT(this) {
-	unserialize(flatv);
-      }
+	{
+	  unserialize(flatv);
+	}
 
       size_t size() const {
 	lock_guard guard(mtx);
-	return keys_view.size();
+	return data.size();
       } /* size */
 
       void clear(uint32_t flags = FLAG_NONE) {
@@ -149,53 +123,35 @@ namespace rgw { namespace bplus {
 	if (likely(! (flags & FLAG_LOCKED))) {
 	  uniq.lock();
 	}
-	keys_view.clear();
-	local_rep.clear();
+	data.clear();
       } /* clear */
 
       int insert(const std::string& key, const std::string& value) {
 	lock_guard guard(mtx);
-	if (keys_view.size() == fanout) {
+	if (data.size() == fanout) {
 	  // oh, noes!  need split
 	  return E2BIG;
 	}
-	keysview_iterator kv_it = std::lower_bound(
-	  keys_view.begin(), keys_view.end(), key, keysviewLT);
-	if (kv_it != keys_view.end()) {
-	  if(unlikely(kv_it->key(this) == key)) {
+	data_iterator kv_it = std::lower_bound(
+	  data.begin(), data.end(), key, keysviewLT);
+	if (kv_it != data.end()) {
+	  if(unlikely(kv_it->key == key)) {
 	    return EEXIST;
 	  }
 	}
-	// append to local rep vector (TODO: journal)
-	local_rep.emplace_back(key, value);
 	// now use kv_it to do a positional insert into keys_view
-	keys_view.insert(kv_it, KeyEntry({local_rep.size()-1}));
+	data.insert(kv_it, {key, value});
 	return 0;
       } /* insert */
 
       int remove(const std::string& key) {
 	lock_guard guard(mtx);
-	// TODO:  variant backing (local_rep and flatbuffer */
-	keysview_iterator kv_it = std::lower_bound(
-	  keys_view.begin(), keys_view.end(), key, keysviewLT);
-	if (kv_it != keys_view.end() &&
-	    kv_it->key(this) == key) {
-	  /* remove from local_rep--to do this, swap the key to be
-	   * deleted with the last element in the vector, then erase
-	   * the element in last position */
-	  auto& elt = kv_it->elt(this);
-	  auto& elt2 = local_rep.back();
-	  /* sadly, we don't know (though could save at the cost
-	   * of significant space) the position of the keys_view
-	   * record that points to local_rep.back(), and need it
-	   * to execute the swap step */
-	  keysview_iterator back_it = std::lower_bound(
-	    keys_view.begin(), keys_view.end(), elt2.key, keysviewLT);
-	  std::swap(elt, elt2);
-	  local_rep.pop_back();
-	  back_it->rep_off = kv_it->rep_off;
-	  /* now erase kv_it from the sorted view */
-	  keys_view.erase(kv_it);
+	// TODO:  variant backing (local_rep and flatbuffer) */
+	data_iterator kv_it = std::lower_bound(
+	  data.begin(), data.end(), key, keysviewLT);
+	if (kv_it != data.end() &&
+	    kv_it->key == key) {
+	  data.erase(kv_it);
 	}
 	return 0;
       } /* remove */
@@ -213,18 +169,18 @@ namespace rgw { namespace bplus {
 	  uniq.lock();
 	}
 
-	keysview_iterator it = (prefix)
-	  ? std::lower_bound(keys_view.begin(), keys_view.end(), *prefix,
-			    keysviewLT)
-	  : keys_view.begin();
-	for (; it != keys_view.end() && count < lim; ++it) {
-	  auto k = it->key(this);
+	data_iterator it = (prefix)
+	  ? std::lower_bound(data.begin(), data.end(), *prefix,
+			     keysviewLT)
+	  : data.begin();
+	for (; it != data.end() && count < lim; ++it) {
+	  auto k = it->key;
 	  // stop iteration iff prefix search and prefix not found
 	  if (prefix && (flags & FLAG_REQUIRE_PREFIX) &&
 	      !ba::starts_with(k, *prefix)) {
 	    goto out;
 	  }
-	  auto ret = cb(&k, &it->val(this));
+	  auto ret = cb(&k, &it->val);
 	  ++count;
 	}
       out:
@@ -286,8 +242,7 @@ namespace rgw { namespace bplus {
 	  // TODO:  verify sharing rep (i.e., flatv)
 	  auto key = kv_data[kv_ix].AsString().c_str();
 	  auto val = kv_data[kv_ix+1].AsString().c_str();
-	  local_rep.emplace_back(key, val);
-	  keys_view.emplace_back(local_rep.size()-1);
+	  data.emplace_back(key, val);
 	}
 	// except keys_view is already sorted!
 	//std::sort(keys_view.begin(), keys_view.end(), keysviewLT);
